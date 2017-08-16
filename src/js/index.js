008e1cf8e6f63cd6564744781aec52c0a9193c83
FallenMax <FallenMax@gmail.com>
2017-08-16 22:13:14 +0800

    unlock more evil restrict

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
-document.documentElement.addEventListener('copy', e => e.stopPropagation(), {
-  capture: true
-})
+// unblock copy-relevant events
+const events = [
+  'copy',
+  'cut',
+  'contextmenu',
+  'selectstart',
+  'dragstart',
+  'drag',
+  'dragend'
+]
+events.forEach(evt =>
+  document.documentElement.addEventListener(evt, e => e.stopPropagation(), {
+    capture: true
+  })
+)
+
+// unblock keypress events
+const keyEvents = ['keydown', 'keypress', 'keyup']
+keyEvents.forEach(evt =>
+  document.documentElement.addEventListener(
+    evt,
+    e => (e.ctrlKey || e.metaKey || e.key === 'F12') && e.stopPropagation(),
+    {
+      capture: true
+    }
+  )
+)
+
+// allow select in css
+const style = document.createElement('STYLE')
+style.innerHTML = ' * { user-select: initial !important; } '
+document.head.append(style)
