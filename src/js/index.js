66e15bbb64ca46736a9d908f62efe31d96342258
FallenMax <FallenMax@gmail.com>
2017-10-29 11:12:32 +0800

    hijack Event.prototype to block 'preventDefault;

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,46 @@
-// unblock copy-relevant events
-const events = [
-  'copy',
-  'cut',
-  'contextmenu',
-  'selectstart',
-  'dragstart',
-  'drag',
-  'dragend'
-]
-events.forEach(evt =>
-  document.documentElement.addEventListener(evt, e => e.stopPropagation(), {
-    capture: true
-  })
-)
-
-// unblock keypress events
-const keyEvents = ['keydown', 'keypress', 'keyup']
-keyEvents.forEach(evt =>
-  document.documentElement.addEventListener(
-    evt,
-    e => (e.ctrlKey || e.metaKey || e.key === 'F12') && e.stopPropagation(),
-    {
+function unblock() {
+  const directCopyEvents = ['copy', 'cut', 'contextmenu', 'selectstart']
+  const rejectOtherHandlers = e => {
+    e.stopPropagation()
+    if (e.stopImmediatePropagation) e.stopImmediatePropagation()
+  }
+  directCopyEvents.forEach(evt => {
+    document.documentElement.addEventListener(evt, rejectOtherHandlers, {
       capture: true
+    })
+  })
+
+  const indirectCopyEvents = [
+    'mousedown',
+    'mouseup',
+    'mousemove',
+    'keydown',
+    'keypress',
+    'keyup'
+  ]
+  const oldPrevent = Event.prototype.preventDefault
+  Event.prototype.preventDefault = function() {
+    if (indirectCopyEvents.indexOf(this.type) !== -1) {
+      console.info(
+        `[allow-copy] webpage attempting to block "${this.type}" event, ignored`
+      )
+    } else {
+      oldPrevent.apply(this, arguments)
     }
-  )
-)
+  }
+}
+try {
+  const script = document.createElement('SCRIPT')
+  script.id = 'allow-copy_script'
+  script.textContent = `(${unblock})()`
+  document.documentElement.append(script)
+} catch (error) {
+  console.warn(`[allow-copy] failed to inject script`, error)
+}
 
 // allow select in css
 const style = document.createElement('STYLE')
-style.innerHTML = ' * { user-select: initial !important; } '
-if (document.head) document.head.append(style)
+style.id = 'allow-copy_style'
+style.innerHTML =
+  ' html,body,*,*::before,*::after { -webkit-user-select: initial; user-select: initial !important; } '
+document.documentElement.append(style)
