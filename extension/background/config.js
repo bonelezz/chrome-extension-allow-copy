6918236c8e81d8389d643ca7c08990672651d8e6
FallenMax <FallenMax@gmail.com>
2018-06-01 17:48:32 +0800

    use local storage to avoid out of quota issue

--- extension/background/config.js
+++ extension/background/config.js
@@ -1,13 +1,13 @@
 const configManager = (() => {
   const get = key => {
     return new Promise((resolve, reject) => {
-      chrome.storage.sync.get([key], result => resolve(result[key]))
+      chrome.storage.local.get([key], result => resolve(result[key]))
     })
   }
 
   const set = (key, value) => {
     return new Promise((resolve, reject) => {
-      chrome.storage.sync.set({ [key]: value }, resolve)
+      chrome.storage.local.set({ [key]: value }, resolve)
     })
   }
 
